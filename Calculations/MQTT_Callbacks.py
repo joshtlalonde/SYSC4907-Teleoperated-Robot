ecfc645f47e76d951672e9be27b23f658e867e1e
Josh <joshtlalonde@cmail.carleton.ca>
2023-11-25 18:20:55 -0500

    Update to use the MQTT_Callbacks The main is used for testing

--- Calculations/MQTT_Callbacks.py
+++ Calculations/MQTT_Callbacks.py
@@ -1,68 +1,56 @@
-# import paho.mqtt.client as mqtt
+import json
 
-# class MQTT_Callbacks:
-#     def __init__(self):
-#         pass
+from MQTT_Client import MQTT_Client
+import paho.mqtt.client as mqtt
 
-#     def on_connect(self, client: mqtt.Client, userdata, flags, rc):
-#         if rc == 0:
-#             print(f"Connected to MQTT Broker: {rc}")
-#         elif rc == 1:
-#             print(f"Connection refused - incorrect protocol version: {rc}")
-#         elif rc == 2:
-#             print(f"Connection refused - invalid client identifier: {rc}")
-#         elif rc == 3:
-#             print(f"Connection refused - server unavailable: {rc}")
-#         elif rc == 4:
-#             print(f"Connection refused - bad username or password: {rc}")
-#         elif rc == 5:
-#             print(f"Connection refused - not authorised: {rc}")
+def on_connect(client: mqtt.Client, userdata: MQTT_Client, flags, rc):
+    if rc == 0:
+        print(f"<MQTT_Callbacks>: Connected to MQTT Broker: {rc}")
+    elif rc == 1:
+        print(f"<MQTT_Callbacks>: Connection refused - incorrect protocol version: {rc}")
+    elif rc == 2:
+        print(f"<MQTT_Callbacks>: Connection refused - invalid client identifier: {rc}")
+    elif rc == 3:
+        print(f"<MQTT_Callbacks>: Connection refused - server unavailable: {rc}")
+    elif rc == 4:
+        print(f"<MQTT_Callbacks>: Connection refused - bad username or password: {rc}")
+    elif rc == 5:
+        print(f"<MQTT_Callbacks>: Connection refused - not authorised: {rc}")
+        # Attempt to reconnect
+        client.reconnect()
 
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"flags: {flags}")
-#         print(f"rc: {rc}")
+def on_disconnect(client: mqtt.Client, userdata: MQTT_Client, rc):
+    if rc == 0:
+        print(f"<MQTT_Callbacks>: Disconnected from MQTT Broker: {rc}")
+    else:
+        print(f"<MQTT_Callbacks>: Unexpected Disconnection from MQTT Broker: {rc}")
+        # Attempt to reconnect
+        client.reconnect()
 
-#     def on_disconnect(self, client: mqtt.Client, userdata, rc):
-#         if rc == 0:
-#             print(f"Disconnected from MQTT Broker: {rc}")
-#         else:
-#             print(f"Unexpected Disconnection from MQTT Broker: {rc}")
+def on_publish(client: mqtt.Client, userdata: MQTT_Client, mid):
+    print(f"<MQTT_Callbacks>: Published to topic with message id: {mid}")
 
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"rc: {rc}")
+def on_subscribe(client: mqtt.Client, userdata: MQTT_Client, mid, granted_qos):
+    print(f"<MQTT_Callbacks>: Subscribed to topic with message id: {mid} and qos: {granted_qos}")
 
-#     def on_publish(self, client: mqtt.Client, userdata, mid):
-#         print(f"Published to topic with message id: {mid}")
+def on_unsubscribe(client: mqtt.Client, userdata: MQTT_Client, mid):
+    print(f"<MQTT_Callbacks>: Unsubscribed to topic with message id: {mid}")
 
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"mid: {mid}")
+def on_message(client: mqtt.Client, userdata: MQTT_Client, message: mqtt.MQTTMessage):
+    print(f"<MQTT_Callbacks>: Received message on topic {message.topic}: {message.payload}")
 
-#     def on_subscribe(self, client: mqtt.Client, userdata, mid, granted_qos):
-#         print(f"Subscribed to topic with message id: {mid} and qos: {granted_qos}")
+def on_encoder(client: mqtt.Client, userdata: MQTT_Client, message: mqtt.MQTTMessage):
+    if userdata.client_id not in message.topic:
+        print(f"<MQTT_Callbacks>: Received ENCODER message on topic {message.topic}: {message.payload}")
 
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"mid: {mid}")
-#         print(f"granted_qos: {granted_qos}")
+        if "trainee" in message.topic:
+            print(f"    Updating 'trainee' encoder value: {message.payload.decode()}")
+            userdata.test_enc = json.loads(message.payload.decode())
 
-#     def on_unsubscribe(self, client: mqtt.Client, userdata, mid):
-#         print(f"Unsubscribed to topic with message id: {mid}")
+def on_current(client: mqtt.Client, userdata: MQTT_Client, message: mqtt.MQTTMessage):
+    if userdata.client_id not in message.topic:
+        print(f"<MQTT_Callbacks>: Received CURRENT message on topic {message.topic}: {message.payload}")
 
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"mid: {mid}")
-
-#     def on_message(self, client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
-#         print(f"Received message on topic {message.topic}: {message.payload}")
-
-#         print(f"client: {client}")
-#         print(f"userdata: {userdata}")
-#         print(f"message: {message}")
-
-#     def on_test_message(self, client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
-#         print(f"Received TEST message on topic {message.topic}: {message.payload}")
-
-        
+        if "trainee" in message.topic:
+            print(f"    Updating 'trainee' current value: {message.payload.decode()}")
+            userdata.test_curr = json.loads(message.payload.decode())
--- Calculations/MQTT_Client.py
+++ Calculations/MQTT_Client.py
@@ -1,11 +1,14 @@
 import os
 import time
+import json
 
 import paho.mqtt.client as mqtt
-# from MQTT_Callbacks import MQTT_Callbacks
+import MQTT_Callbacks as callbacks
 
 # TODO: Create a Logger??
 
+
+# TODO: If we were to have the kinematics CLASS connected into this then we can update it directly within the message functions
 class MQTT_Client:
     def __init__(self, cert_path: str, client_id: str = None):        
         # Create MQTT Client object
@@ -14,23 +17,29 @@ class MQTT_Client:
         self.client.tls_set(cert_path)
         # Keep track of client name
         self.client_id = client_id
+        # Setup the userdata for callbacks
+        self.client.user_data_set(self)
 
-        # Attach the Client Callbacks class to handle the callbacks
-        # self.callbacks = self.MQTT_Callbacks()
+        # TODO: Setup a self.kin for the kinematics class, this way we can just update the encoder and current values within the messages
+        self.test_enc = {'left': 0, 'right': 0}
+        self.test_curr = {'left': 0, 'right': 0}
 
-        ### CONNECT ###
         # Setup callback function for when connection is made
-        self.client.on_connect = self.on_connect
+        self.client.on_connect = callbacks.on_connect
         # Setup callback function for when disconnection occurs
-        self.client.on_disconnect = self.on_disconnect
+        self.client.on_disconnect = callbacks.on_disconnect
         # Setup callback function for when publish request occurs
-        self.client.on_publish = self.on_publish
+        self.client.on_publish = callbacks.on_publish
         # Setup callback function for when subscribe request is made
-        self.client.on_subscribe = self.on_subscribe
+        self.client.on_subscribe = callbacks.on_subscribe
         # Setup callback function for when unsubscribe request occurs
-        self.client.on_unsubscribe = self.on_unsubscribe
+        self.client.on_unsubscribe = callbacks.on_unsubscribe
         # Setup callback function for when message not in filter list occurs
-        self.client.on_message = self.on_message
+        self.client.on_message = callbacks.on_message
+        # Setup callback function for when encoder message is received
+        self.client.message_callback_add(f"encoder/#", callbacks.on_encoder)
+        # Setup callback function for when current message is received
+        self.client.message_callback_add(f"current/#", callbacks.on_current)
 
     # Connect to Broker and initialize listing loop
     def connect(self, broker_hostname: str, broker_port: int = 8883):
@@ -45,18 +54,18 @@ class MQTT_Client:
         try:
             messageInfo: mqtt.MQTTMessageInfo = self.client.publish(topic, payload, qos, retain)
         except ValueError:
-            raise ValueError("qos is not 0, 1 or 2.",
+            raise ValueError("<MQTT_Client>: qos is not 0, 1 or 2.",
                              "Or topic is None, has zero length or is invalid (contains a wildcard).",
                              "Or if the length of the payload is greater than 268435455 bytes.")
         
         if messageInfo.rc == mqtt.MQTT_ERR_SUCCESS:
-            print(f"{self.client_id} successfully published to {topic}",
+            print(f"<MQTT_Client>: {self.client_id} successfully published to {topic}",
                   f"with qos {qos} and message id {messageInfo.mid}. Message Content: {payload}")
         elif messageInfo.rc == mqtt.MQTT_ERR_NO_CONN:
-            print(f"{self.client_id} failed to publish to {topic}",
+            print(f"<MQTT_Client>: {self.client_id} failed to publish to {topic}",
                   f"with qos {qos} and message id {messageInfo.mid}. Client is not currently connected")
         else:
-            print(f"{self.client_id} failed to publish to {topic}",
+            print(f"<MQTT_Client>: {self.client_id} failed to publish to {topic}",
                   f"with qos {qos} and message id {messageInfo.mid}.")
 
 
@@ -64,109 +73,63 @@ class MQTT_Client:
         try:
             (result, mid) = self.client.subscribe(topic, qos)
         except ValueError:
-            raise ValueError("qos is not 0, 1 or 2.",
+            raise ValueError("<MQTT_Client>: qos is not 0, 1 or 2.",
                              "Or topic is None or has zero string length.",
                              "Or topic is not a string, tuple or list.")
         
         if result == mqtt.MQTT_ERR_SUCCESS:
-            print(f"{self.client_id} successfully subscribed to {topic}",
+            print(f"<MQTT_Client>: {self.client_id} successfully subscribed to {topic}",
                   f"with qos {qos}. Message ID: {mid}")
         else:
-            print(f"Client {self.client_id} failed to subscribe to {topic}",
+            print(f"<MQTT_Client>: Client {self.client_id} failed to subscribe to {topic}",
                   f"with qos {qos}. Message ID: {mid}")
 
     def unsubscribe(self, topic: str):
         try:
             (result, mid) = self.client.unsubscribe(topic)
         except ValueError:
-            raise ValueError("Topic is None or has zero string length.",
+            raise ValueError("<MQTT_Client>: Topic is None or has zero string length.",
                              "Or topic is not a string, tuple or list.")
         
         if result == mqtt.MQTT_ERR_SUCCESS:
-            print(f"{self.client_id} successfully unsubscribed to {topic}.",
+            print(f"<MQTT_Client>: {self.client_id} successfully unsubscribed to {topic}.",
                   f"Message ID: {mid}")
         else:
-            print(f"{self.client_id} failed to unsubscribed to {topic}.",
+            print(f"<MQTT_Client>: {self.client_id} failed to unsubscribed to {topic}.",
                   f"Message ID: {mid}")
             
     def message_callback_add(self, sub: str, callback):
         self.client.message_callback_add(sub, callback)
 
-
-######################## CALLBACK FUNCTIONS ########################
-### TODO: Find a way to have these in a separate file for easier reading
-
-    def on_connect(self, client: mqtt.Client, userdata, flags, rc):
-        if rc == 0:
-            print(f"{self.client_id} Connected to MQTT Broker: {rc}")
-        elif rc == 1:
-            print(f"{self.client_id} Connection refused - incorrect protocol version: {rc}")
-        elif rc == 2:
-            print(f"{self.client_id} Connection refused - invalid client identifier: {rc}")
-        elif rc == 3:
-            print(f"{self.client_id} Connection refused - server unavailable: {rc}")
-        elif rc == 4:
-            print(f"{self.client_id} Connection refused - bad username or password: {rc}")
-        elif rc == 5:
-            print(f"{self.client_id} Connection refused - not authorised: {rc}")
-
-    def on_disconnect(self, client: mqtt.Client, userdata, rc):
-        if rc == 0:
-            print(f"{self.client_id} Disconnected from MQTT Broker: {rc}")
-        else:
-            print(f"{self.client_id} Unexpected Disconnection from MQTT Broker: {rc}")
-
-    def on_publish(self, client: mqtt.Client, userdata, mid):
-        print(f"{self.client_id} Published to topic with message id: {mid}")
-
-    def on_subscribe(self, client: mqtt.Client, userdata, mid, granted_qos):
-        print(f"{self.client_id} Subscribed to topic with message id: {mid} and qos: {granted_qos}")
-
-    def on_unsubscribe(self, client: mqtt.Client, userdata, mid):
-        print(f"{self.client_id} Unsubscribed to topic with message id: {mid}")
-
-    def on_message(self, client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
-        print(f"{self.client_id} Received message on topic {message.topic}: {message.payload}")
-
-    def on_test_message(self, client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
-        print(f"{self.client_id} Received TEST message on topic {message.topic}: {message.payload}")
-
-        self.publish(f"respond/{self.client_id}", f"How is it going from {self.client_id}", qos=1)
-
 if __name__ == "__main__":
+    ##### TESTING #####
     dir_path = os.path.dirname(os.path.realpath(__file__)) + r"\certs\ca-root-cert.crt"
     # Create Clients
-    client1 = MQTT_Client(cert_path=dir_path, client_id="Client1")
-    client2 = MQTT_Client(cert_path=dir_path, client_id="Client2")
+    client1 = MQTT_Client(cert_path=dir_path, client_id="kin")
     # Connect Clients
     print("Connect Client 1")
     client1.connect(broker_hostname="LAPTOP-HL4N9U5Q")
-    print("Connect Client 2")
-    client2.connect(broker_hostname="LAPTOP-HL4N9U5Q")
+
     # Subscribe Clients
     print("Subscribe Client 1")
-    client1.subscribe(f"test/{client2.client_id}")
-    client1.subscribe(f"respond/{client2.client_id}")
-    print("Subscribe Client 2")
-    client2.subscribe(f"test/{client1.client_id}")
-    client2.subscribe(f"respond/{client1.client_id}")
-    
-    # Create Test message Filter
-    print("Create filter Client 1")
-    client1.message_callback_add("test/#", client1.on_test_message)
-    print("Create filter Client 2")
-    client2.message_callback_add("test/#", client2.on_test_message)
+    client1.subscribe(f"encoder/#")
+    client1.subscribe(f"current/#")
 
     time.sleep(1)
 
-    # Take turns publishing
-    print("Publish Client 1")
-    client1.publish(f"test/{client1.client_id}", "Hello From Client1", qos=1)
-    print("Publish Client 2")
-    client2.publish(f"test/{client2.client_id}", "Hello From Client2", qos=1)
+    for i in range(10):
+        client1.test_enc['left'] = client1.test_enc['left'] + 1000
+        client1.test_enc['right'] = client1.test_enc['right'] + 100
+        client1.test_curr['left'] = client1.test_curr['left'] + 10
+        client1.test_curr['right'] = client1.test_curr['right'] + 1 
+
+        # Take turns publishing
+        print("Publish Client 1")
+        client1.publish(f"encoder/{client1.client_id}", json.dumps(client1.test_enc), qos=1)
+        client1.publish(f"current/{client1.client_id}", json.dumps(client1.test_curr), qos=1)
     
-    time.sleep(1)
+        time.sleep(5)
 
     # Disconnect clients
     client1.disconnect()
-    client2.disconnect()
+    # client2.disconnect()
