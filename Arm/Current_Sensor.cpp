56e547ed43ca97075f98565fa89587c84a66d2aa
Josh <joshtlalonde@cmail.carleton.ca>
2024-03-03 17:42:44 -0500

    Initialize the Current Sensor

--- Arm/Current_Sensor.cpp
+++ Arm/Current_Sensor.cpp
@@ -1,23 +1,46 @@
 
 #include "Current_Sensor.h"
 
-Current_Sensor::Current_Sensor(int pin)
+Current_Sensor::Current_Sensor(int pin, float voltage_offset)
 {
-    // Initialize Current Value
+    // Initialize Values
+    this->pin = pin;
     this->current = 0; 
     this->target = 0;
+    this->voltage_offset = voltage_offset;
 
     /* Set Pin Defintions */
     pinMode(pin, INPUT);
 }
 
 float Current_Sensor::getCurrent() {
-    /** TODO: Read this value from analog pin? */
-    return this->current;
-}
+    const int avgSamples = 10; // Number of samples to average the reading over
+    const float sensitivity = 155.0 / 150.0; //155mA per 150mV 
+    int sensorValue = 0; 
+    float Vref = 1500 ; // Output voltage with no current: ~ 1500mV or 1.5V
 
-void Current_Sensor::setCurrent(float current) {
-    this->current = current;
+    // read the analog in value:
+    for (int i = 0; i < avgSamples; i++)
+    {
+        sensorValue += analogRead(this->pin);
+
+        // wait 2 milliseconds before the next loop
+        // for the analog-to-digital converter to settle
+        // after the last reading:
+        delay(2);
+    }
+
+    sensorValue = sensorValue / avgSamples;
+
+    // The on-board ADC is 13-bits -> 2^13 = 8192 -> 3.3V / 8192 ~= 0.402mV
+    // The voltage is in millivolts
+    float voltage = 0.402 * sensorValue - this->voltage_offset; // 428 is offset to get to approx. 1500mV (Gain??)
+
+    // This will calculate the actual current (in mA)
+    // Using the Vref and sensitivity settings you configure
+    float current = (voltage - Vref) * sensitivity;
+
+    return current;
 }
 
 float Current_Sensor::getTarget() {
@@ -27,3 +50,8 @@ float Current_Sensor::getTarget() {
 void Current_Sensor::setTarget(float target) {
     this->target = target;
 }
+
+
+
+
+
--- Arm/Current_Sensor.h
+++ Arm/Current_Sensor.h
@@ -6,16 +6,36 @@
 //Create a Motor Control Class
 class Current_Sensor {
     private:
+        int pin;
         float current;
         float target;
+        float voltage_offset;
         
     public: 
-        Current_Sensor(int pin);
+        Current_Sensor(int pin, float voltage_offset);
 
-        float getCurrent();
-        void setCurrent(float current);
         float getTarget();
         void setTarget(float target);
+
+        /*  SparkFun ACS712 and ACS723 Demo
+            Created by George Beckstein for SparkFun
+            4/30/2017
+            Updated by SFE
+            6/14/2018
+
+            Uses an Arduino to set up the ACS712 and ACS723 Current Sensors
+            See the tutorial at: https://learn.sparkfun.com/tutorials/current-sensor-breakout-acs723-hookup-guide
+
+            Parts you may need:
+            - 100 Ohm, 1/2W or greater resistor OR two 220 Ohm 1/4 resistors in parallel
+            - ACS712 Breakout with on-board amplifier or ACS723 Current Sensor (Low Current)
+
+            Optional equipment:
+            - Oscilloscope
+            - Multimeter (or two)
+            - A power supply with current limiting/constant current would be handy to calibrate the device without using resistors
+        */
+        float getCurrent();
 };
 
 #endif
